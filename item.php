7c198ed737cd7e0eb7bee67e280c65568d97e8dd
BlurplePanda <bootham@wgc.school.nz>
2021-09-28 10:25:59 +1300

    Added item description

--- item.php
+++ item.php
@@ -31,10 +31,11 @@ if(mysqli_connect_errno()){
 
     if(isset($_GET['id'])){
         $id = $_GET['id'];
-        $this_item_query = "SELECT itemName, itemPrice, itemInStock, typeName FROM items, itemtypes WHERE itemID = '".$id."' AND items.typeID = itemtypes.typeID";
+        $this_item_query = "SELECT items.*, typeName FROM items, itemtypes WHERE itemID = '".$id."' AND items.typeID = itemtypes.typeID";
         $this_item_result = mysqli_query($con, $this_item_query);
         $this_item_record = mysqli_fetch_assoc($this_item_result);
         echo "<p> Item Name: ".$this_item_record['itemName'];
+        echo "<p> Item description: ".$this_item_record['itemDescription'];
         echo "<p> Type: ".$this_item_record['typeName'];
         echo "<p> Cost: $".$this_item_record['itemPrice'];
         echo "<p> Availability: ";
