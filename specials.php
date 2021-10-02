459c3c99259ffdda59c9f31963e0d47ebdc76c24
BlurplePanda <bootham@wgc.school.nz>
2021-10-03 12:17:22 +1300

    Clarified special meaning and added new price calculation

--- specials.php
+++ specials.php
@@ -3,7 +3,7 @@ $con = mysqli_connect("localhost", "bootham", "richpatch76", "bootham_canteen");
 if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
 
-$specials_query = "SELECT weekDay, items.itemID, itemName FROM specials, items WHERE specials.itemID = items.itemID";
+$specials_query = "SELECT weekDay, items.itemID, itemName, itemPrice FROM specials, items WHERE specials.itemID = items.itemID";
 $specials_result = mysqli_query($con, $specials_query);
 
 ?><!DOCTYPE html>
@@ -29,10 +29,12 @@ $specials_result = mysqli_query($con, $specials_query);
 
 <main>
     <h1>Specials</h1>
+    <p>50% off the specified item on each day!
     <?php
     while($specials_record = mysqli_fetch_assoc($specials_result)){
         echo "<p>".$specials_record['weekDay'].": ";
         echo "<a href='item.php?id=".$specials_record['itemID']."'>".$specials_record['itemName']."</a>";
+        echo "<p>Normally $".$specials_record['itemPrice'].", but only $".sprintf("%.2f\n", $specials_record['itemPrice']*0.5)." on ".$specials_record['weekDay']."s!";
     }
     ?>
 
